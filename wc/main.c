0362ec5862919df07094f4718b0b4f7697b50409
Joseph Thorne <joseph.thorne@student.uq.edu.au>
2025-11-12 17:22:37 +1000

    initial commit, boiler plate and test.txt

---
+++ wc/main.c
@@ -0,0 +1,30 @@
+/*
+ * ccwc
+ * 12/11/25
+ *
+ */
+
+// Constants and Data structures
+const char* const countBytesArg = "-c";
+const char* const usageMessage = "Usage: ccwc -c file\n";
+
+typedef struct Method {
+  unsigned char bytes;
+  unsigned char lines;
+  unsigned char words;
+  unsigned char chars;
+} Method;
+// Prototypes
+
+
+int main(int argc, char* argv[]) {
+  // check command line
+  Method method;
+
+  // open file
+   
+  // read/count
+  
+  // output
+  return 0;
+}
