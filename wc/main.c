52b4910d9625b05d8ffdfbebf3c615dfcea2cf5e
Joseph Thorne <joseph.thorne@student.uq.edu.au>
2025-11-12 20:35:21 +1000

    processCommandLine() done

--- wc/main.c
+++ wc/main.c
@@ -7,26 +7,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BYTE 1
+#define LINE 2
+#define WORD 4
+#define CHAR 8
+
+const char* const byteArg = "-c";
+const char* const lineArg = "-l";
+const char* const wordArg = "-w";
+const char* const charArg = "-m";
+
 // Constants and Data structures
 const char* const countBytesArg = "-c";
-const char* const usageMessage = "Usage: ccwc -c file\n";
+const char* const noFileMessage = "To be implemented, for now you must specify a filename\n";
 
-typedef struct CommandLine {
-  unsigned char bytes;
-  unsigned char lines;
-  unsigned char words;
-  unsigned char chars;
+typedef struct Parameters {
+  unsigned char outputs;
   char* filename;
-} CommandLine;
+} Parameters;
 
 // Prototypes
-void getCL(int argc, char* argv[], CommandLine* command); 
+Parameters processCommandLine(int argc, char* argv[], CommandLine* command); 
 
 int main(int argc, char* argv[]) {
   // check command line
-  CommandLine command;
-  getCL(argc, argv, &command);
-  printf("filename is %s\n", command.filename);
+  Parameters params = processCommandLine(argc, argv, &params);
   // open file
     
   // read/count
@@ -35,17 +40,36 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-void getCL(int argc, char* argv[], CommandLine* command) {
-  
-  switch (argc) {
-    case 1:
-      printf(usageMessage);
-      exit(1);
-    case 2: // only filename
-      command->bytes = 1;
-      command->lines = 1;
-      command->words = 1;
-      command->chars = 1;
-      command->filename = argv[1];
+Parameters processCommandLine(int argc, char* argv[], Parameters* params) {
+
+  Parameters params {
+    .outputs = 0,
+    .filename = NULL
+  };
+
+  // Skip program name
+  argc--;
+  argv++;
+
+  while (argv[0] && strncmp(argv[0], "-", 1) == 0) {
+    // current argument begins with '-'
+    if (!strcmp(argv[0], byteArg)) {
+      params.outputs = params.outputs | BYTE;
+    } else if (!strcmp(argv[0], lineArg)) {
+      params.outputs = params.outputs | LINE;
+    } else if (!strcmp(argv[0], wordArg)) {
+      params.outputs = params.outputs | WORD;
+    } else if (!strcmp(argv[0], charArg)) {
+      params.outputs = params.outputs | CHAR;
+    }
+    // Skip one argument
+    argc--;
+    argv++;
   }
+
+  if (argv[0]) {
+    params.filename = argv[0];
+  }
+
+  return params;
 } 
